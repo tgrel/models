d2634cddd7e98eb4bd83a7bfe636afbeab06138b
Tomasz Grel <tgrel@nvidia.com>
2019-09-06 16:06:25 +0200

    WIP

--- official/recommendation/ncf_common.py
+++ official/recommendation/ncf_common.py
@@ -164,8 +164,6 @@ def define_ncf_flags():
       enable_xla=True,
       force_v2_in_keras_compile=True,
       fp16_implementation=True,
-      loss_scale=True,
-      dynamic_loss_scale=True,
   )
   flags_core.define_device(tpu=True)
   flags_core.define_benchmark()
